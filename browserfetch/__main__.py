66cc925075f1a4d4d28912a2696e701cd402f1b0
5j9 <5j9@users.noreply.github.com>
2025-01-17 06:39:28 +0330

    feat: expose copyjs

--- browserfetch/__main__.py
+++ browserfetch/__main__.py
@@ -12,24 +12,25 @@ def read_js(host_name_generator: str | None):
     return js
 
 
+def copyjs(*, host_name_generator: str | None = None):
+    """Copy contents of browserfetch.js to clipboard.
+
+    `host_name_generator` should be a string containing
+    an async JavaScript function named `generateHostName()`. This file's
+    contents will be copied into the generated js script and will be used
+    to generate a host name for connections. The default
+    `generateHostName` function returns `location.host`.
+    """
+    from pyperclip import copy
+
+    copy(read_js(host_name_generator))
+
+
 if __name__ == '__main__':
     from cyclopts import App
-    from pyperclip import copy
 
     from browserfetch import __version__
 
     app = App(version=__version__)
-
-    @app.command
-    def copyjs(*, host_name_generator: str | None = None):
-        """Copy contents of browserfetch.js to clipboard.
-
-        `host_name_generator` should be a string containing
-        an async JavaScript function named `generateHostName()`. This file's
-        contents will be copied into the generated js script and will be used
-        to generate a host name for connections. The default
-        `generateHostName` function returns `location.host`.
-        """
-        copy(read_js(host_name_generator))
-
+    app.command(copyjs)
     app()
